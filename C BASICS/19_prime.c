764c717272556a7fb00e4e2fee93eee74dde040c
Ravi Kumar Singh <onlyravi4321@gmail.com>
2025-11-09 18:29:10 +0530

    Optimize prime number check with early returns
    Added early return for non-prime cases to improve efficiency.

--- C BASICS/19_prime.c
+++ C BASICS/19_prime.c
@@ -6,9 +6,11 @@ int main() {
     scanf("%d", &num);
     if ( num <= 1 ){
         printf("%d is not a prime number.",num);
+        return 0;
     }
     else if ( num == 2 ){
         printf("%d is a prime number.",num);
+        return 0;
     }
     for(i = 2; i < num ; i++) {
         if(num % i == 0) {
@@ -21,4 +23,5 @@ int main() {
     else
         printf("%d is not a prime number.\n", num);
     return 0;
+
 }
