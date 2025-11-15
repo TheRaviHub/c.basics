8e057cd6ffc637bd21a233ede7c148848f8053bc
Ravi Kumar Singh <onlyravi4321@gmail.com>
2025-11-15 22:31:31 +0530

    Add comments for clarity in powerRaised function

--- 05 RECURSION/05_basePower.c
+++ 05 RECURSION/05_basePower.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int powerRaised(int a, int b){
     if(b==0) return 1;
-    int result = a * powerRaised(a,b-1);
+    int result = a * powerRaised(a,b-1);   //recursion 
     return result;
 }
 int main(){
     int a,b;
     printf("Enter the base = ");
-    scanf("%d",&a);
+    scanf("%d",&a);  //a = base
 
     printf("Enter the power = ");
-    scanf("%d",&b);
+    scanf("%d",&b);   //b = power
 
-    int result = powerRaised(a,b);
-    printf("%d to the power %d = %d",a,b,result);
+    int result = powerRaised(a,b);   //function call
+    printf("%d to the power %d = %d",a,b,result);   //output
 }
